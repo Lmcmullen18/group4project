f3686dbb7d937bd3c05044aa9cb6b89762275bf6
Nihan Durmaz <naldis@gmail.com>
2022-09-01 13:50:16 -0500

    Sep1st changes

--- js/index.js
+++ js/index.js
@@ -1,38 +1,46 @@
-const task1 = new TaskManager('task');
-console.log(task1.tasks);
+// const task1 = new TaskManager('task');
+// console.log(task1.tasks);
 
+const validFormFieldInput = (data) => {
+    if (data !== null && data !== "") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let newTaskForm = document.querySelector("#newTaskForm");
 newTaskForm.addEventListener('submit', (event) => {
     // preventDefault action
     event.preventDefault();
-    
+    console.log(event);
+
     //select the inputs
-    const newtasknameinput = document.querySelector("#taskname");
-    const newtaskndescription = document.querySelector("#taskdescr");
-    const newtaskassignedto = document.querySelector("#Assigned");
-    const newtaskduedate = document.querySelector("#start");
-    const newtaskmessage = document.querySelector("#alertmess");
+    const newTaskNameInput = document.querySelector("#taskname");
+    const newTaskDescription = document.querySelector("#taskdescr");
+    const newTaskAssignedto = document.querySelector("#Assigned");
+    const newTaskDueDate = document.querySelector("#start");
+    const errorMessage = document.querySelector(".alert");
+    console.log(newTaskNameInput);
 
+    // validation code 
 
+    // get the value of the form inputs
 
+    const taskName = newTaskNameInput.value;
+    const description = newTaskDescription.value;
+    const assignedTo = newTaskAssignedto.value;
+    const dueDate = newTaskDueDate.value;
+    console.log(taskName);
 
-// validation code 
-
-// get the value of the inputs
-
-const name = newtasknameinput.value;
-const description = newtaskndescription.value;
-const assignedTo = newtaskassignedto.value;
-const dueDate = newtaskduedate.value;
-if(!validFormFieldInput(name)){
-    errorMessage.innerHTML = "Invalid name input";
-    errorMessage.style.display = "block";
-} else{
-    errorMessage.style.display = "none";
-
-}
+    if (!validFormFieldInput(taskName) || !validFormFieldInput(description) || !validFormFieldInput(assignedTo) || !validFormFieldInput(dueDate)) {
+        errorMessage.innerHTML = "Invalid input";
+        errorMessage.style.display = "block";
+        alert("Error!")
+    } else {
+        alert("submission is completed!");
+    }
 
 });
-
-function validFormFieldInput(data){
-    return data !== null && data !== '';
-}
+const textHtml = createTaskHtml(taskName, description, dueDate, assignedTo);
+console.log(textHtml);
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,21 +1,47 @@
+function createTaskHtml(taskName, description, dueDate, assignedTo) {
+
+    const html = `
+    <div class="card" style="width: 18rem; ">
+    <div class="card-body">
+      <h5 class="card-title" id="task1">Task name: ${taskName}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">task 1 description : ${description}</h6>
+      <p class="card-text"> Assigned to : ${assignedTo}</p>
+      <p class="card-text"> Due Date: ${dueDate}</p>
+      <button  type="button" class="btn btn-danger">IN PROGRESS  
+      </button >
+      <button type="button" class="btn btn-dark">Delete</button></div>  
+    </div>
+  </div>
+    `
+}
+
 class TaskManager {
-    constructor(currentId = 0){
+    constructor(currentId = 0) {
         this.tasks = [];
         this.currentId = currentId;
     }
 
     //create the addtask method
 
-     addTask(name,description,assignedTo, dueDate){
-         const task = {
-             // increment the currentid property
-             id: this.currentId++,
-             name: name,
-             description: description,
-             dueDate: dueDate,
-             status:'TODO'
-         };
-         // push the task to the task property
-         this.tasks.push(task);
-     }
+    addTask(taskName, description, assignedTo, dueDate) {
+        const task = {
+            // increment the currentid property
+            id: this.currentId++,
+            name: taskName,
+            description: description,
+            dueDate: dueDate,
+            status: 'TODO'
+        };
+        // push the task to the task property
+        this.tasks.push(task);
+    }
+    render() {
+        let tasksHtmlList = [];
+        for (let i = 0; i < tasksHtmlList.length; i++) {
+            currentTask = i;
+            console.log(currentTask);
+        }
+        let date = new Date(dueDate)
+        let formattedDate = 
+    }
 }
